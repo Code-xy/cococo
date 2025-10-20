cabb52a8a41e5df0d8c0acf10acd2a00b4f43f68
李老斯 <57938533+Code-xy@users.noreply.github.com>
2025-10-20 22:18:00 +0800

    Update dianxin2.js

--- dianxin2.js
+++ dianxin2.js
@@ -18,7 +18,7 @@
 
 # > 中国电信充值记录拦截，删除最新的第一条记录
 # 适配接口：充值明细查询
-^https?:\/\/.*\.189\.cn\/.* url script-response-body https://raw.githubusercontent.com/Code-xy/cococo/refs/heads/main/dianxin2.js
+^https?:\/\/[^\/]+\.189\.cn\/.+ url script-response-body https://raw.githubusercontent.com/Code-xy/cococo/refs/heads/main/dianxin2.js
 
 [mitm] 
 hostname = *.189.cn
